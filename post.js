e254ab432cbae9bc8c6f1814728a4c188c9de41c
Edouard Adjaro <ediadjaro710@gmail.com>
2021-12-31 11:39:56 +0000

    npm  avec mongosse et react

---
+++ post.js
@@ -0,0 +1,26 @@
+const  posts = [
+    {
+        id: 1,
+        tite:"My blog post 2",
+        content:"Carlos santana",
+        category:"technology",
+        createAt:"2020-03-13T16: 08: 57.092+00: 00"
+    },
+
+    {
+        id: 2,
+        tite:"My blog post 3",
+        content:"Carlos santana",
+        category:"technology",
+        createAt:"2020-03-13T18: 06: 27.092+00: 00"
+    },
+    {
+        id: 3,
+        tite:"My blog post 1",
+        content:"Carlos santana",
+        category:"technology",
+        createAt:"2020-03-13T19:07:36.811+00: 00"
+    },
+]
+
+module.exports=posts
